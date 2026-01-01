f17742f1fcc9e3c10e30311b0a917d56319ccb90
tatamyiwathy <jbd00353@gmail.com>
2026-01-02 01:36:12 +0900

    getDirectionLightDirメソッドの条件を修正し、方向光がない場合の戻り値を[0, 0, 0]に変更

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -34,12 +34,9 @@ export class Renderer {
     }
 
     getDirectionLightDir(gl, scene) {
-        // const directionalLights = scene.lights.filter( light => light. lightKind === "directional" );
-        // const lightsDir = directionalLights.reduce( ( acc, light) => vec3.add(acc, light.direction), [0,0,0]);
-        if( scene.lights.length == 0 ) {
-            return new Float32Array([0, -1, 0]);
+        if (scene.lights.length == 0 || scene.lights[0].lightKind != "directional") {
+            return new Float32Array([0, 0, 0]);
         }
-        // directionは正規化されている
         return scene.lights[0].direction;
     }
 
